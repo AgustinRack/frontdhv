4b3efc750b075db614794cb29c2d2169f9a8e4a4
Tu Nombre <agustinrack14@hotmail.com>
2023-08-04 16:39:54 -0300

    signup

--- src/assets/readme.md
+++
@@ -1,1 +0,0 @@
-aca van a ir todas las imagenes y logos que va a tener la aplicacion
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,11 +1,57 @@
 import React from "react";
 import "../login/Login.css";
 import loginImage from "../../assets/loginImage.jpg";
+import fullLogo from "../../assets/fullLogo.svg";
+import vector from "../../assets/Vector.svg";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { Link } from "react-router-dom";
 
 function Login() {
   return (
-    <div className="login-left-styles">
-      <img className="login-image-style" src={loginImage} alt="house image" />
+    <div className="login-container">
+      <img className="login-image-style" src={loginImage} alt="house" />
+
+      <div className="login-card">
+        <div className="login-card-top">
+          <img className="fullLogo-style" src={fullLogo} alt="full logo" />
+          <div className="vector-container">
+            <img className="vector-style" src={vector} alt="vector" />
+            <div className="vector-text">Tu nueva vivienda esta aquí</div>
+          </div>
+        </div>
+        <div>
+          <Form>
+            <Form.Group className="mb-3 email-field" controlId="formBasicEmail">
+              <Form.Control
+                className="field-style"
+                type="email"
+                placeholder="EMAIL"
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3 password-field"
+              controlId="formBasicPassword"
+            >
+              <Form.Control
+                className="field-style"
+                type="password"
+                placeholder="PASSWORD"
+              />
+            </Form.Group>
+
+            <div className="change-password">
+              <Link to="/signup">
+                <p>Crear cuenta</p>
+              </Link>
+            </div>
+
+            <Button className="login-button" variant="primary" type="submit">
+              Login
+            </Button>
+          </Form>
+        </div>
+      </div>
     </div>
   );
 }
